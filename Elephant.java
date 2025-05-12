0545c4667da469fe5104574c5672082ace31243b
DhirenSutha <348599382@gapps.yrdsb.ca>
2025-05-11 23:55:27 -0400

    Part 2 update

--- Elephant.java
+++ Elephant.java
@@ -19,7 +19,7 @@ public class Elephant extends Actor
     
     String facing = "right";
     
-    
+    SimpleTimer animationTimer = new SimpleTimer();
     public Elephant()
     {
         for(int i = 0; i < idleRight.length; i++)
@@ -33,11 +33,19 @@ public class Elephant extends Actor
             idleLeft[i].mirrorHorizontally();
             idleLeft[i].scale(75, 75);
         }
+        
+        animationTimer.mark();
+        
         setImage(idleRight[0]);
     }
     int imageIndex = 0;
     public void animateElephant()
     {
+        if(animationTimer.millisElapsed() < 300)
+        {
+            return;
+        }
+        animationTimer.mark();
         if(facing.equals("right"))
         {
             setImage(idleRight[imageIndex]);
